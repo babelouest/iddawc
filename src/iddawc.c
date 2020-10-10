15acac05f70825e99d1c824793455e7ed3268320
Nicolas Mora <github@babelouest.org>
2020-10-10 18:23:15 -0400

    Implement global functions

--- src/iddawc.c
+++ src/iddawc.c
@@ -829,16 +829,17 @@ static int _i_add_token_authentication(struct _i_session * i_session, struct _u_
 }
 
 int i_global_init() {
-  if (ulfius_send_request_init() == U_OK) {
+  if (ulfius_global_init() == U_OK && r_global_init() == RHN_OK) {
     return RHN_OK;
   } else {
-    y_log_message(Y_LOG_LEVEL_ERROR, "i_global_init - Error ulfius_send_request_init");
+    y_log_message(Y_LOG_LEVEL_ERROR, "i_global_init - Error ulfius_send_request_init or r_global_init");
     return RHN_ERROR;
   }
 }
 
 void i_global_close() {
-  ulfius_send_request_close();
+  ulfius_global_close();
+  r_global_close();
 }
 
 int i_init_session(struct _i_session * i_session) {
