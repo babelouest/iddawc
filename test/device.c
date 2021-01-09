c0eb359c9486fbf12e25b43647e93ad46138421a
Nicolas Mora <github@babelouest.org>
2021-01-09 10:31:41 -0500

    Disable logs

--- test/device.c
+++ test/device.c
@@ -293,7 +293,7 @@ int main(int argc, char *argv[])
   int number_failed;
   Suite *s;
   SRunner *sr;
-  y_init_logs("Iddawc", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting Iddawc device tests");
+  //y_init_logs("Iddawc", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting Iddawc device tests");
   i_global_init();
   s = iddawc_suite();
   sr = srunner_create(s);
@@ -303,6 +303,6 @@ int main(int argc, char *argv[])
   srunner_free(sr);
   
   i_global_close();
-  y_close_logs();
+  //y_close_logs();
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
